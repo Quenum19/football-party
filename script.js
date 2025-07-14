fcb418994e58afa5a7090d7a24693511a66ac741
Quenum19 <sioquenum75@gmail.com>
2025-07-14 15:20:04 +0000

    Nouveau projet : remplacement complet

--- script.js
+++
@@ -1,9 +0,0 @@
-function downloadBadge() {
-  const badge = document.getElementById("badge");
-  html2canvas(badge).then(canvas => {
-    const link = document.createElement('a');
-    link.download = 'badge-saveurs-sunday.png';
-    link.href = canvas.toDataURL("image/png");
-    link.click();
-  });
-}
